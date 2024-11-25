6863b7e3cd32e4066331422dec94effe6c3b1e28
Luis <luis.fergon.2@educa.jcyl.es>
2024-11-25 19:11:16 +0100

    25/11

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--Autor: Luis Ferreras González
+    15/10-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+        <link type="text/css" rel="stylesheet" href="webroot/estilos.css">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <button>Log in</button>
+        </header>
+        <main>
+            <h2>Index</h2>
+        </main>
+        <footer>
+            <a href="../208DWESProyectoDWES/indexProyectoDWES.php">Luis Ferreras</a>
+            <a href="">GitHub</a>
+        </footer>
+    </body>
+</html>
